a0a6c3a097728fb2dfb842d5b6f3056a3652b022
Pedro Carvalho <carvalho.pedro@usp.br>
2022-12-04 11:47:19 -0300

    inicio da pagina dinamica

--- movies/views.py
+++ movies/views.py
@@ -7,3 +7,7 @@ def detail_movie(request, movie_id):
     movie = movie_data[movie_id - 1]
     return HttpResponse(
         f'Detalhes do filme {movie["name"]} ({movie["release_year"]})')
+
+def list_movies(request):
+    context = {"movie_list": movie_data}
+    return render(request, 'movies/index.html', context)
