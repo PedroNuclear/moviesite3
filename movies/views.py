2b07956fb8a61ccd088ebb8a406dad5c71657060
Pedro Carvalho <carvalho.pedro@usp.br>
2022-12-04 15:31:19 -0300

    ultimo tentativa de dinamico funcionando

--- movies/views.py
+++ movies/views.py
@@ -4,10 +4,13 @@ from django.http import HttpResponse
 from .temp_data import movie_data
 
 def detail_movie(request, movie_id):
-    movie = movie_data[movie_id - 1]
-    return HttpResponse(
-        f'Detalhes do filme {movie["name"]} ({movie["release_year"]})')
+    context = {'movie': movie_data[movie_id - 1]}
+    return render(request, 'movies/detail.html', context)
 
 def list_movies(request):
     context = {"movie_list": movie_data}
     return render(request, 'movies/index.html', context)
+
+#def list_movies(request):
+#   return HttpResponse("<h1>Hello World!</h1>")
+
--- staticpages/urls.py
+++ staticpages/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 
 from . import views
 
+
 urlpatterns = [
     path('', views.index, name='index'),
     path('about/', views.about, name='about'),
-    path('<int:movie_id>/', views.detail, name='detail'),
 ]
